b845ba4582ce2c8f2eecfa5a15ce1b1d248acc8d
Vyacheslav Dudko <happaro@gmail.com>
2016-05-15 02:53:25 +0300

    Big rat head occupation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,28 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
-public enum EnemyType { Mario, Following }
+public enum EnemyType {SmallRat, BigRat}
 
 public class EnemyController : MonoBehaviour
 {
-
     public float speed = 10f;
     public float attackRange = 2;
     public float health = 100;
+	public float jumpForce = 2;
+
     public int power = 10;
     public EnemyType type;
+	public Transform headPlace;
+
+	public SpriteRenderer occupatedSprite;
     private Animator anim;
-    private Rigidbody2D rigi;
+	public BoxCollider2D groundChecker;
+	[HideInInspector]
+	public Rigidbody2D rigi;
     private PlayerController player;
     private float distance;
-    // Use this for initialization
+	public bool isOccupated = false;
     void Start()
-    {
+	{
         anim = GetComponent<Animator>();
         rigi = GetComponent<Rigidbody2D>();
         player = GameObject.FindObjectOfType<PlayerController>();
     }
-
-    // Update is called once per frame
+		
     void Update()
     {
         Move();
@@ -30,19 +35,16 @@ public class EnemyController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.tag == "Player")
+		if (coll.gameObject.tag == "Player" && !isOccupated)
         {
             StartCoroutine(Hit());
             player.Jump();
-        }
-        
+        }   
     }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.tag == "Killer")
-            Destroy(gameObject);
-        if (coll.gameObject.tag == "Fliper" && type == EnemyType.Mario)
+		if (coll.gameObject.tag == "Fliper" && type == EnemyType.SmallRat)
         {
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
@@ -57,23 +59,29 @@ public class EnemyController : MonoBehaviour
 
     IEnumerator Hit()
     {
-        anim.SetBool("Kick", true);
+//        anim.SetBool("Kick", true);
         player.GetDamage(power);
         
         yield return new WaitForSeconds(0.2f);
-        anim.SetBool("Kick", false);
+  //      anim.SetBool("Kick", false);
     }
 
     void Move()
     {
-       if(type == EnemyType.Following)
+		if(type == EnemyType.BigRat)
         {
-            if(Mathf.Abs(player.transform.position.x - transform.position.x)>1)
+            if (Mathf.Abs(player.transform.position.x - transform.position.x)>1)
                 rigi.velocity = new Vector2(speed * (int)(Mathf.Sign(player.transform.position.x - transform.position.x)), rigi.velocity.y);
         }
-       else if (type == EnemyType.Mario)
+		else if (type == EnemyType.SmallRat)
         {
-            rigi.velocity = new Vector2(speed*Mathf.Sign(transform.localScale.x), rigi.velocity.y);
+            rigi.velocity = new Vector2(speed * Mathf.Sign(transform.localScale.x), rigi.velocity.y);
         }
     }
+
+	public void Jump()
+	{
+		rigi.velocity = new Vector2(rigi.velocity.x, 0);
+		rigi.AddForce(new Vector2(0, jumpForce));
+	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,55 +6,94 @@ public class PlayerController : MonoBehaviour
     public float speed = 10f;
     public float jumpForce = 600f;
     public bool isFacingRight = true;
-    public Transform groundCheck;
     public float groundRadius = 0.2f;
-    public LayerMask whatIsGround;
+	public BoxCollider2D groundChecker;
+	public SpriteRenderer mySprite;
 
     public float health = 100;
     private Animator anim;
     private Rigidbody2D rigi;
     private bool isGrounded = true;    
-    
+	public SpriteRenderer mainSprite, occupatedSpriteRat, occupatedSpriteBrain;
+	private Transform currentTransform;
+	private bool occupating;
+	private Collider2D currentCollider;
+
     private void Start()
     {
         anim = GetComponent<Animator>();
         rigi = GetComponent<Rigidbody2D>();
+		currentTransform = transform;
     }
+
+	void HeadOccupation(EnemyController enemy)
+	{
+		this.transform.position = new Vector3(enemy.transform.position.x, -2.8f, this.transform.position.z);
+		Destroy(enemy.gameObject);
+		rigi.velocity = Vector2.zero;
+		mainSprite.enabled = false;
+		occupatedSpriteRat.enabled = true;
+		occupatedSpriteBrain.enabled = true;
+	}
+
+	void JumpAndUnoccupate()
+	{
+		this.transform.Translate(0, 1, 0);
+		mainSprite.enabled = true;
+		occupatedSpriteRat.enabled = false;
+		occupatedSpriteBrain.enabled = false;
+		rigi.AddForce(new Vector2(0, jumpForce));
+		occupating = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.tag == "BigRatHead")
+		{
+			currentCollider = coll;
+		}
+		else if (coll.gameObject.tag == "Enemy")
+		{
+			if (coll.gameObject.GetComponent<EnemyController>().type == EnemyType.SmallRat)
+				Destroy(coll.gameObject);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D coll)
+	{
+		currentCollider = null;
+	}
+
+	private void Update()
+	{
+		//JUMP
+		if (Input.GetButtonDown("Jump") && groundChecker.IsTouchingLayers(LayerMask.GetMask("Ground")))
+			Jump();	
+		if (!occupating)
+		{
+			if (Input.GetKeyDown(KeyCode.G) && currentCollider != null && currentCollider.tag == "BigRatHead")
+			{
+				//currentCollider.transform.parent.GetComponent<EnemyController>().isOccupated = true;
+				occupating = true;
+				HeadOccupation(currentCollider.transform.parent.GetComponent<EnemyController>());
+			}
+		}
+		else
+		{
+			if (Input.GetKeyDown(KeyCode.G))
+				JumpAndUnoccupate();
+		}
+	}
+
     private void FixedUpdate()
     {
-        //Runing
-        float move = Input.GetAxis("Horizontal");
-        if (Mathf.Abs(move) > 0.3) move = 1 * Mathf.Sign(move);
-//        anim.SetFloat("hSpeed", Mathf.Abs(move));
-        rigi.velocity = new Vector2(move * speed, rigi.velocity.y);
-        if (move > 0 && !isFacingRight)
-            Flip();
-        else if (move < 0 && isFacingRight)
-            Flip();
-        //Jumping
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-//        anim.SetBool("Ground", isGrounded);
-//        anim.SetFloat("vSpeed", rigi.velocity.y);
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (isGrounded)
-            {
-                Jump();
-            }            
-        }
-        
-    }
-    private void Update()
-    {
+		float move = Input.GetAxis("Horizontal");
+		if (move != 0)
+			currentTransform.localScale = Mathf.Abs(currentTransform.localScale.x) * new Vector3(move > 0 ? 1 : -1, 1, 1);
+		//RUN
+		rigi.velocity = new Vector2(move * speed, rigi.velocity.y);
 
-    }
 
-    private void Flip()
-    {
-        isFacingRight = !isFacingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
     }
 
     public void Jump()
