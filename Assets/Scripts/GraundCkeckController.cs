5b366aa14f03052b5b05bc00fb8dbc8642901b7c
Dima Lupyak <dima.lupyak@gmail.com>
2016-05-14 18:39:06 +0300

    Added player and enemy controllers

---
+++ Assets/Scripts/GraundCkeckController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraundCkeckController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            gameObject.GetComponentInParent<PlayerController>().Jump();
+        }
+            
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpForce = 600f;
+    public bool isFacingRight = true;
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    private Animator anim;
+    private Rigidbody2D rigi;
+    private bool isGrounded = true;    
+    
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigi = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        //Runing
+        float move = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(move) > 0.3) move = 1 * Mathf.Sign(move);
+        anim.SetFloat("hSpeed", Mathf.Abs(move));
+        rigi.velocity = new Vector2(move * speed, rigi.velocity.y);
+        if (move > 0 && !isFacingRight)
+            Flip();
+        else if (move < 0 && isFacingRight)
+            Flip();
+        //Jumping
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", isGrounded);
+        anim.SetFloat("vSpeed", rigi.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                Jump();
+            }            
+        }
+    }
+    private void Update()
+    {
+
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public void Jump()
+    {
+        rigi.velocity = new Vector2(rigi.velocity.x, 0);
+        rigi.AddForce(new Vector2(0, jumpForce));
+    }
+}
