7af988e9d9e5387e72e29d3d5eb6647aa81ba6d0
Vyacheslav Dudko <happaro@gmail.com>
2016-05-14 21:48:59 +0300

    First steps

---
+++ Assets/Scripts/SmoothTranslate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothTranslate : MonoBehaviour 
+{
+	public Transform mainObj;
+	public float speed = 0.2f;
+	void Update () 
+	{
+		this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, mainObj.localPosition + new Vector3 (3, -mainObj.localPosition.y, -10), speed);
+	}
+}
