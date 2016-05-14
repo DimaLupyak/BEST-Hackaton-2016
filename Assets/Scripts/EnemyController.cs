512a23a1109df70b782caa89cf3e3effbfa9505d
Dima Lupyak <dima.lupyak@gmail.com>
2016-05-14 21:04:56 +0300

    Added enemy moving

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+public enum EnemyType { Mario, Following }
 
 public class EnemyController : MonoBehaviour
 {
 
-
+    public float speed = 10f;
     public float attackRange = 2;
     public float health = 100;
     public int power = 10;
-    public float hitDelta = 50;
+    public EnemyType type;
     private Animator anim;
     private Rigidbody2D rigi;
     private PlayerController player;
@@ -24,6 +25,7 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
     }
 
     void OnCollisionEnter2D(Collision2D coll)
@@ -33,13 +35,19 @@ public class EnemyController : MonoBehaviour
             StartCoroutine(Hit());
             player.Jump();
         }
+        
     }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Killer")
             Destroy(gameObject);
-
+        if (coll.gameObject.tag == "Fliper" && type == EnemyType.Mario)
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
 
     public void GetDamage(int damage)
@@ -54,5 +62,18 @@ public class EnemyController : MonoBehaviour
         
         yield return new WaitForSeconds(0.2f);
         anim.SetBool("Kick", false);
+    }
+
+    void Move()
+    {
+       if(type == EnemyType.Following)
+        {
+            if(Mathf.Abs(player.transform.position.x - transform.position.x)>1)
+                rigi.velocity = new Vector2(speed * (int)(Mathf.Sign(player.transform.position.x - transform.position.x)), rigi.velocity.y);
+        }
+       else if (type == EnemyType.Mario)
+        {
+            rigi.velocity = new Vector2(speed*Mathf.Sign(transform.localScale.x), rigi.velocity.y);
+        }
     }
 }
