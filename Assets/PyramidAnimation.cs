8c4df36002cffe4688c3f6618ecb845fe9104076
Vyacheslav Dudko <happaro@gmail.com>
2016-05-15 10:28:59 +0300

    Levels scene

---
+++ Assets/PyramidAnimation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Holoville.HOTween;
+
+public class PyramidAnimation : MonoBehaviour 
+{
+	public Transform cam;
+	public int currentLevel = 1;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.RightArrow) && currentLevel < 19)
+		{
+			currentLevel++;
+			MoveMapToLevel(currentLevel);
+		}
+		else if(Input.GetKeyDown(KeyCode.LeftArrow) && currentLevel > 1)
+		{
+			currentLevel--;
+			MoveMapToLevel(currentLevel);
+		}
+	}
+
+	void Start () 
+	{
+		FirstAnimation();
+	}
+
+	void FirstAnimation()
+	{
+		TweenParms parms = new TweenParms();
+		parms.Prop("rotation", new Vector3(0, 180, 0));
+		HOTween.To(cam.parent, 8, parms);
+		HOTween.To(cam, 8, "localPosition", new Vector3(0, 30, -120));
+	}
+
+	void MoveMapToLevel(int level)
+	{
+		HOTween.Kill(cam.parent);
+		HOTween.Kill(cam);
+		HOTween.To(cam.parent, 2, "rotation", new Vector3(0, 180 + ((level - 1) % 6) * (-360f / 6f) , 0));
+		HOTween.To(cam, 2, "localPosition", new Vector3(0, 15 + (level - 1) / 6 * (100 / 3), -120));
+	}
+}
